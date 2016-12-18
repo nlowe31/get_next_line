02bb87f323f7e0914d7822878d090383218cf7b4
Nate Lowe <nlowe31@gmail.com>
2016-12-18 22:40:50 +0100

    squashing bugs

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 20:51:31 by nlowe             #+#    #+#             */
-/*   Updated: 2016/12/18 13:22:15 by nlowe            ###   ########.fr       */
+/*   Updated: 2016/12/18 22:33:41 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*a;
+	unsigned char	*temp;
 
-	a = (unsigned char *)b;
+	temp = (unsigned char *)b;
 	while (len-- > 0)
-		*a++ = (unsigned char)c;
-	return (a);
+		*temp++ = (unsigned char)c;
+	return (b);
 }
--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 23:01:29 by nlowe             #+#    #+#             */
-/*   Updated: 2016/12/17 15:54:54 by nlowe            ###   ########.fr       */
+/*   Updated: 2016/12/18 22:38:27 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,5 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 		dst[i] = '\0';
 		i++;
 	}
-	dst[i] = '\0';
 	return (dst);
 }
